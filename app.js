9cd7d0c7008ea8e06f3611ae713a807bdf3960e9
cesar <cesaruni84@gmail.com>
2023-12-15 23:49:29 -0500

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/hello", function (req, res) {
   res.send("IT WORKED");
 });
 app.get("/new", function (req, res) {
